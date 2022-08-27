2f4a56143a76573c111c5bbbcb0301e1e12cd117
leduararaujo <leduar.araujo@hotmail.com>
2022-08-27 00:37:28 -0300

    Implementação do rest da consulta das noticias do adrenaline

--- server/route/adrenaline.js
+++ server/route/adrenaline.js
@@ -3,43 +3,16 @@ const router = express.Router();
 
 router.get('/consulta-noticias', (req, res) => {
 
-        var decode = 'https://v1.nocodeapi.com/danish/xml_to_json/vjoGHCzVsMVoIXLV?url=';
         var url = 'https://adrenaline.com.br/rss';
+        var axios = require('axios');
+        var fs = require('fs');
+        var { parseString } = require('xml2js');
 
-        //Preparativos para a requisição
-        var https = require('https');
-
-        /*soap.createClient(url, (err, client) => {
-                client.consultaCEP({}, (error, result) => {
-                        res.status(200).json(result);
-                })
-        });*/
-
-        https.get(decode + url, (response) => {
-                if (response.statusCode == 200) {
-
-                        response.on('data', odd => {
-                                let corpoXML = odd.toString('utf8');
-                                let retorno = Buffer.from(JSON.stringify(odd, 'utf8'));
-                                res.status(200).json(retorno);
-
-                                /*xml2js.parseString(corpoXML, (err, result) => {
-                                        let json = JSON.stringify(result, null, 4);
-                                        //console.log(json);
-                                });*/
-                                //console.log(json);
-                                
-                        });
-
-                        /*response.on('data', (body) => {
-                                let corpo = convert.xml2json(
-                                        xml.readFileSync(body, 'utf-8'));
-                                res.status(200).send(corpo);                    
-                        });*/
-                        
-                }
-        });
-
+        axios.get(url).then((response) => {
+                parseString(response.data, (err, jsonBody) => {
+                        res.status(200).json(jsonBody);
+                });
+        }).catch( err => res.status(500).json(err));
 });
 
 module.exports = router;
